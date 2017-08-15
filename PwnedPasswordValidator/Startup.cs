4e15b3076cdf3cd8d6f591d7b6a5dae6ce43729a
Richard Perry <richard@einfinity.co.uk>
2017-08-15 11:48:58 +0100

    Add project files.

---
+++ PwnedPasswordValidator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PwnedPasswordValidator.Startup))]
+namespace PwnedPasswordValidator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
